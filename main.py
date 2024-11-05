dc716dafb67d7b1183cca5a6d49c0c57ded1305e
beaudrytinklerl <beaudrytinklerl@educ.college-montreal.qc.ca>
2024-11-05 14:40:09 -0500

    commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ groupe: 401
 création de personnages DnD
 """
 from random import randint, choice
+from dataclasses import dataclass
 
 
 def de_six():
@@ -17,16 +18,19 @@ def de_six():
     atribut.clear()
     return resultat
 
+
 def noms():
     liste_nom = ["Bob", "Serge", "Marcus", "Jean-Yve", "Marie-Anne", "Sir Sacàmèche", "Adrienne"]
     nom = choice(liste_nom)
     return nom
+
 
 def professions():
     liste_professions = ["barbare", "barde", "clerc", "druide", "ensorceleur", "guerrier", "magicien", "moine", "occultiste", "paladin", "rôdeur", "roublard"]
     profession = choice(liste_professions)
     return profession
 
+@dataclass
 class NPC:
     def __init__(self, force, agilite, constitution, intelligence, sagesse, charisme, class_armur, nom, race, espece, pv, proffession):
         self.force = force
@@ -52,20 +56,26 @@ class Kobold(NPC):
         super().__init__(de_six(), de_six(), de_six(), de_six(), de_six(), de_six(), randint(1, 12), noms(), "elf", "elf noir", randint(1, 20), professions())
 
     def attaquer(self):
-
-
-    def subir_dommage(self):
+        self.cible = NPC
+        atteindre = randint(1, 20)
+        de_huit = randint(1, 8)
+        if atteindre == 1:
+            print("\nvous avez roulez un 1.\nVouz écouez lamentablement")
+        elif atteindre == 20:
+            print(f"\nBravo!Vouz avez roulez un 20.\nVous faites {de_huit} points de dommage à l'ennemi")
+            cible = self.pv - de_huit
+        else:
+            if atteindre >= NPC().class_armur:
+                print("ca marche")
 
 
 class Hero(NPC):
     def __init__(self):
         super().__init__(de_six(), de_six(), de_six(), de_six(), de_six(), de_six(), randint(1, 12), noms(), "elf", "elf noir", randint(1, 20), professions())
 
-    def attaquer(self):
-
-    def subir_dommage(self):
-
-
 
 npc = NPC(de_six(), de_six(), de_six(), de_six(), de_six(), de_six(), randint(1, 12), noms(), "elf", "elf noir", randint(1, 20), professions())
 npc.afficher_caracteristiques()
+
+k = Kobold()
+k.attaquer()
